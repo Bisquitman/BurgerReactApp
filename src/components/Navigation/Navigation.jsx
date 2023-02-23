fec396fa50c36bf3f48db1d489b7295de4c541b2
Bisquitman <lynx8309@gmail.com>
2023-02-23 10:53:17 +0300

    get categories from API server

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,23 +2,29 @@ import style from './Navigation.module.css';
 import {Container} from "../Container/Container";
 import classNames from "classnames";
 import {useDispatch, useSelector} from "react-redux";
-import {changeCategory} from "../../store/category/categorySlice.js";
+import {categoryRequestAsync, changeCategory} from "../../store/category/categorySlice.js";
+import {useEffect} from "react";
+import {API_URL} from "../../const.js";
 
 export const Navigation = () => {
   const {category, activeCategory} = useSelector((state) => state.category);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoryRequestAsync('Burger'))
+  }, []);
 
   return (
     <nav className={style.navigation}>
       <Container className={style.container}>
         <ul className={style.list}>
           {category.map((categoryItem, index) =>
-            <li className={style.item}>
+            <li className={style.item} key={categoryItem.title}>
               <button
                 className={classNames(
                   style.button,
                   activeCategory === index ? style.button_active : '')}
-                style={{backgroundImage: `url(${categoryItem.image})`}}
+                style={{backgroundImage: `url(${API_URL}/${categoryItem.image})`}}
                 onClick={() => {
                   dispatch(changeCategory({indexCategory: index}));
                 }}
